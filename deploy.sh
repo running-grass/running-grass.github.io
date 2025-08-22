120da1b511309d59eabe557c3b6408335a3a25e7
running-grass <hi@grass.show>
2025-08-22 17:49:34 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin main
+git push origin HEAD:main
 
 # Go To Root folder
 cd ..
@@ -36,6 +36,6 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin main
+git push origin HEAD:main
 
 exit 0
